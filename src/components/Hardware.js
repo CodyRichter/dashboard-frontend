8e9fd98a886819dac461ee3d3e5b4e0c59f4427c
Cody Richter <Vortekdynamic@gmail.com>
2021-12-14 17:30:54 -0500

    QR Card

--- src/components/Hardware.js
+++ src/components/Hardware.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import {Typography} from "@mui/material";
 
 export default function Hardware() {
     return(
-        <h2>Hardware</h2>
+        <>
+            <Typography variant='h4' component='div' className='mb-4'>
+                Hardware Inventory
+            </Typography>
+
+        </>
     )
 }
--- src/components/HomePages/ParticipantHome.js
+++ src/components/HomePages/ParticipantHome.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Card, CardContent, Grid, Typography} from "@mui/material";
 import EventLocationCard from "./InfoCards/EventLocationCard";
 import EventRelocationAssistanceCard from "./InfoCards/EventRelocationAssistanceCard";
+import QRCard from "./InfoCards/QRCard";
 
 export default function ParticipantHome(props) {
 
@@ -31,6 +32,10 @@ export default function ParticipantHome(props) {
 
                 <Grid item xs={12} lg={6} xl={4}>
                     <EventRelocationAssistanceCard {...props} />
+                </Grid>
+
+                <Grid item xs={12} lg={6} xl={4}>
+                    <QRCard {...props} />
                 </Grid>
 
             </Grid>
